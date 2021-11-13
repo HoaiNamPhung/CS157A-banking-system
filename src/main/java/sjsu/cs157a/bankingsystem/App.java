f5603b97e21ae78414e851917e1b845a49f7e61b
Hoai-Nam Phung <hoai-nam.phung@sjsu.edu>
2021-11-13 07:39:12 -0800

    Added user registration, login, deletion, sign out features. Added user I/O interaction.

--- src/main/java/sjsu/cs157a/bankingsystem/App.java
+++ src/main/java/sjsu/cs157a/bankingsystem/App.java
@@ -1,23 +1,135 @@
 package sjsu.cs157a.bankingsystem;
 
 import java.sql.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Scanner;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
+public class App {
+	public static void main( String[] args ) throws Exception
     {
+    	// Prepare MySQL connection.
         Connection conn = SQLConnector.getInstance().getConnection();
         if (conn == null) {
-            System.out.println( "No connection." );
+            System.out.println( "No connection to local MySQL server. Please try again later." );
+            throw new SQLException();
         }
         else {
-        	System.out.println( "Connection to MySQL ready." );
-        	Database.createTable(conn, "TestTable", false);
-        	Database.createTable(conn, "TestTable", true);
+        	System.out.println( "Connection to MySQL ready." );	
         }
-    }
+        
+        // User state.   
+        int userID = -1;
+        String firstName = null;
+        String lastName = null;
+        String email = null;
+        LocalTime time = LocalTime.now();
+        LocalDate date = LocalDate.now();
+    	boolean loggedIn = false;
+        
+        // Let user log in.
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Would you like to login(1), register(2), or quit(3)? (Please input 1~3).");
+        boolean inputValid = false;
+        while (!inputValid) {
+        	switch (scanner.nextLine()) {
+        	case "1":
+        		inputValid = true;
+            	while (!loggedIn) {
+                    String pw = null;
+                	System.out.println("Please input your email.");
+                	email = scanner.nextLine();
+                	System.out.println("Please input your password.");
+                	pw = scanner.nextLine();
+                	userID = User.login(conn, email, pw);
+                	if (userID == -1) {
+                		System.out.println("The given email and password do not match for any user within the banking system. Please try again.");
+                	}
+                	else {
+                		loggedIn = true;
+                	}
+            	}
+        		break;
+        	case "2":
+        		inputValid = true;
+            	while (!loggedIn) {
+                    String pw = null;
+                	System.out.println("Please input your first name.");
+                	firstName = scanner.nextLine();
+                	System.out.println("Please input your lastName.");
+                	lastName = scanner.nextLine();
+                	System.out.println("Please input your email.");
+                	email = scanner.nextLine();
+                	System.out.println("Please input your password.");
+                	pw = scanner.nextLine();
+                	userID = User.register(conn, firstName, lastName, email, pw);
+                	if (userID == -2) {
+                		System.out.println("Given email is already in use.");
+                	}
+                	if (userID < 0) {
+                		System.out.println("Registration failed. Please try again.");
+                	}
+                	else {
+                		loggedIn = true;
+                	}
+            	}  
+        		break;
+        	case "3":
+        		inputValid = true;
+        		System.out.println("Quitting.");
+        		break;
+        	default:
+        		System.out.println("Invalid input. Please try again.");
+        		break;
+        	}
+        }    
+        
+        // Logged in message.
+        if (loggedIn) {
+        	System.out.println("Welcome to your personal banking tracker, " + email + ". Today is " + date + ". It is currently " + time + ".");
+        	System.out.println("Please input a number from 1~4 show all actions related to the corresponding category.");
+        }
+        
+        // Banking system usage loop.
+        while (loggedIn) {
+            System.out.println("Accounts (1) | Transactions (2) | Loans (3) | Banks (4) | Delete User (8) | Sign Out (0)");
+            switch (scanner.nextLine()) {
+            case "1":
+            	System.out.println("Not yet implemented.");
+            	break; 
+	        case "2":
+	        	System.out.println("Not yet implemented.");
+	        	break;
+		    case "3":
+		    	System.out.println("Not yet implemented.");
+		    	break;
+		    case "4":
+		    	System.out.println("Not yet implemented.");
+		    	break;
+		    case "8":
+		    	System.out.println("Are you sure? This will delete the user permanently, making all assets inaccessible. (Y/N)");
+		    	String confirmation = scanner.nextLine();
+		    	if (confirmation.toUpperCase().equals("Y")) {
+		    		System.out.println("User '" + email + "' has been deleted.");
+		    		User.deleteUser(conn, userID);
+		    		loggedIn = false;
+		    	}
+		    	else {
+		    		System.out.println("Deletion canceled.");
+		    	}
+		    	break;
+		    case "0":
+		    	System.out.println("Signing out.");
+		    	loggedIn = false;
+            }
+            System.out.println();
+        }
+        
+        // Close scanner on end.
+        scanner.close();
+	}
 }
--- src/main/java/sjsu/cs157a/bankingsystem/Database.java
+++ src/main/java/sjsu/cs157a/bankingsystem/Database.java
@@ -1,7 +1,13 @@
 package sjsu.cs157a.bankingsystem;
 
+import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
 import java.sql.Statement;
+import java.sql.Types;
 
 public class Database {
 	
@@ -25,7 +31,7 @@ public class Database {
 	}
 	
 	/**
-	 * Creates a new table if it doesn't exist. 
+	 * Creates a new table if it doesn't exist. This is a template; the SQL string should be modified to create a specific kind of table.
 	 * @param conn The MySQL connection.
 	 * @param tableName The new table name.
 	 * @param dropTable Flag indicating whether or existing tables of the same name should be dropped beforehand.
@@ -36,9 +42,7 @@ public class Database {
 			String dropSql = "DROP TABLE IF EXISTS " + tableName;
 			String createSql = "CREATE TABLE IF NOT EXISTS " + tableName +
 					"(uid INT PRIMARY KEY AUTO_INCREMENT, " +
-					"first VARCHAR(255), " +
-					"last VARCHAR(255), " +
-					"routingNumber INT)";
+					"sampleText VARCHAR(255)";
 			Statement stmt = conn.createStatement();
 			if (dropTable) {
 				stmt.execute(dropSql);
@@ -50,5 +54,96 @@ public class Database {
 			e.printStackTrace();
 		}
 		return false;
+	}
+	
+	/**
+	 * Creates a new user.
+	 * @param conn The MySQL connection.
+	 * @param firstName The user's first name.
+	 * @param lastName The user's last name.
+	 * @param email The user's email. Must be unique.
+	 * @param pw The user's password.
+	 * @return Returns the new users unique userID. If unique key 'email' is already in use, returns -2. If creation fails for any other reason, returns -1.
+	 */
+	public static int createUser(Connection conn, String firstName, String lastName, String email, byte[] pw) {
+		try {
+			String sql = "CALL CreateUser(?, ?, ?, ?);";
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, firstName);
+			pstmt.setString(2, lastName);
+			pstmt.setString(3, email);
+			pstmt.setBytes(4, pw);
+			// Success check.
+			int rowsUpdated = pstmt.executeUpdate();
+			if (rowsUpdated == 0) {
+				throw new SQLException("No rows affected: user was not created.");
+			}
+			// Return new userID.
+			ResultSet rs = pstmt.executeQuery("SELECT LAST_INSERT_ID()");
+	        if (rs.next()) {
+	        	return rs.getInt(1);
+            }
+            else {
+                throw new SQLException("No rows affected: user was not created.");
+            }
+		}
+		catch (SQLIntegrityConstraintViolationException emailException) {
+			return -2;
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		return -1;
+	}
+	
+	/**
+	 * Deletes a user with a given userID.
+	 * @param conn The MySQL connection.
+	 * @param userID The user's unique user id.
+	 * @return Returns true on successful deletion.
+	 */
+	public static boolean deleteUser(Connection conn, int userID) {
+		try {
+			String sql = "CALL DeleteUser(?);";
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, userID);
+			int rowsUpdated = pstmt.executeUpdate();
+			// Success check.	
+			if (rowsUpdated == 0) {
+				throw new SQLException("No rows affected: user was not deleted.");
+			}
+			return true;
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	/**
+	 * Retrieves a given user's userID.
+	 * @param conn The MySQL connection.
+	 * @param email The user's email.
+	 * @param pw The user's password.
+	 * @return Returns the given user's userID. If query fails or user DNE, returns -1.
+	 */
+	public static int getUserID(Connection conn, String email, byte[] pw) {
+		try {
+			String sql = "CALL GetUserID(?, ?, ?);";
+			CallableStatement cstmt = conn.prepareCall(sql);
+			cstmt.setString(1, email);
+			cstmt.setBytes(2, pw);
+			cstmt.registerOutParameter(3, Types.INTEGER);
+			cstmt.executeUpdate();	
+			int userID = cstmt.getInt(3);
+			if (userID == 0) {
+				throw new SQLException("UserID does not exist for given email and password.");
+			}
+			return userID;
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		return -1;
 	}
 }
